fde192b7a5f8bf26888d2065f1112c84f1145171
jbt <jbt@405_INS>
2022-02-18 10:33:44 +0200

    JPA

--- 18-02-22-jpa-advanced-mapping/src/main/java/app/core/Application9.java
+++ 18-02-22-jpa-advanced-mapping/src/main/java/app/core/Application9.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import app.core.entities.Address;
 import app.core.entities.School;
+import app.core.entities.School.Category;
 import app.core.entities.Student;
 import app.core.entities.Teacher;
 
@@ -19,53 +21,39 @@ public class Application9 {
 
 		EntityManagerFactory factory = ctx.getBean(EntityManagerFactory.class);
 		EntityManager em = factory.createEntityManager();
-		
-//		School school = new School(0, "JBT", Cat, null)
-		
-		Teacher teacher1 = new Teacher(0, "Meir", "Java");
-		Teacher teacher2 = new Teacher(0, "Dina", "Physics");
-		
+
+		Teacher teacher1 = new Teacher(0, "Yosef", "Java");
+		Teacher teacher2 = new Teacher(0, "Zehava", "Physics");
+
 		Student st1 = new Student(0, "aaa", null);
 		Student st2 = new Student(0, "bbb", null);
 		Student st3 = new Student(0, "ccc", null);
 		Student st4 = new Student(0, "ddd", null);
 		Student st5 = new Student(0, "eee", null);
-		
+
 		// connect teachers and students
 		teacher1.addStudent(st1);
 		teacher1.addStudent(st2);
 		teacher1.addStudent(st3);
-		
+
 		teacher2.addStudent(st4);
 		teacher2.addStudent(st5);
-		
+
+		Address address = new Address(0, "Israel", "Jerusalem", "center", "herzel");
+		School school = new School(0, "JBT", Category.PRIVATE, address);
+		school.addTeacher(teacher1);
+		school.addTeacher(teacher2);
+
 		try {
 			em.getTransaction().begin();
-			// save the students
-//			em.persist(st1);
-//			em.persist(st2);
-//			em.persist(st3);
-//			em.persist(st4);
-//			em.persist(st5);
-			
-			// save the teachers
-//			em.persist(teacher1);
-//			em.persist(teacher2);
-
-			// save the teachers with loop
-			Teacher[] arr = {teacher1, teacher2};
-			for (Teacher teacher : arr) {
-				em.persist(teacher);
-			}
-			
+			em.persist(school);
 			em.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
 			em.getTransaction().rollback();
-		}finally {
+		} finally {
 			em.close();
 		}
-
 
 	}
 
