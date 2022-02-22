9a75b1d5380b540caa9b3fc6a3d0c8069e23e652
jbt <jbt@405_INS>
2022-02-22 20:41:24 +0200

    aaa

--- 22-02-22-spring-rest-eldar/src/main/java/app/core/controllers/UserController.java
+++ 22-02-22-spring-rest-eldar/src/main/java/app/core/controllers/UserController.java
@@ -4,18 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.core.beans.User;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
 	private int nextId = 101;
@@ -24,7 +26,8 @@ public class UserController {
 
 //	POST - create new resource on the server
 	// MIME type
-	@PostMapping(path = "/add", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+//	@PostMapping(path = "/add", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@PostMapping
 	public int addUser(@RequestBody User user) {
 		user.setId(nextId++);
 		this.users.add(user);
@@ -33,7 +36,7 @@ public class UserController {
 	}
 
 //	GET - get an existing resource from the server
-	@GetMapping("/get/{userId}")
+	@GetMapping("/{userId}")
 	public ResponseEntity<?> getUser(@PathVariable int userId) {
 		User user = new User();
 		user.setId(userId);
@@ -48,8 +51,13 @@ public class UserController {
 		}
 	}
 
+	@GetMapping
+	public List<User> getAllUsers() {
+		return this.users;
+	}
+
 //	UPDATE - update an existing resource on the server
-	@PutMapping("/update")
+	@PutMapping
 	public ResponseEntity<Void> updateUser(@RequestBody User user) {
 		int index = users.indexOf(user);
 		if (index != -1) {
@@ -63,6 +71,11 @@ public class UserController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 	}
-//	DELTE - delete an existing resource from the server
+
+//	DELETE - delete an existing resource from the server
+	@DeleteMapping("/{userId}")
+	public boolean delete(@PathVariable int userId) {
+		return users.remove(new User(userId, null, null));
+	}
 
 }
