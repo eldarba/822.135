ae720c07951951d47f8e508da32a6c391c60bea8
jbt <jbt@405_INS>
2022-02-22 19:53:16 +0200

    aaa

--- 22-02-22-spring-rest-eldar/src/main/java/app/core/beans/User.java
+++ 22-02-22-spring-rest-eldar/src/main/java/app/core/beans/User.java
@@ -2,11 +2,13 @@ package app.core.beans;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
 
 	private int id;
--- 22-02-22-spring-rest-eldar/src/main/java/app/core/controllers/UserController.java
+++ 22-02-22-spring-rest-eldar/src/main/java/app/core/controllers/UserController.java
@@ -3,8 +3,13 @@ package app.core.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,10 +33,36 @@ public class UserController {
 	}
 
 //	GET - get an existing resource from the server
-	public User getUser(int userId) {
-		return null;
+	@GetMapping("/get/{userId}")
+	public ResponseEntity<?> getUser(@PathVariable int userId) {
+		User user = new User();
+		user.setId(userId);
+		int index = users.indexOf(user);
+		if (index != -1) {
+			user = users.get(index);
+			return ResponseEntity.ok(user); // response status 200 OK + body with user
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("getUser faild - not found");
+			// ???
+			// throw new RuntimeException("getUser faild - not found");
+		}
 	}
+
 //	UPDATE - update an existing resource on the server
+	@PutMapping("/update")
+	public ResponseEntity<Void> updateUser(@RequestBody User user) {
+		int index = users.indexOf(user);
+		if (index != -1) {
+			User oldUser = users.get(index);
+			oldUser.setEmail(user.getEmail());
+			oldUser.setName(user.getName());
+			System.out.println(users);
+			// return new ResponseEntity<>(HttpStatus.OK);
+			return ResponseEntity.status(HttpStatus.OK).build();
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
 //	DELTE - delete an existing resource from the server
 
 }
