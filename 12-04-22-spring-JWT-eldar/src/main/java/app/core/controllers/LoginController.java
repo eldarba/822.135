09a6f1059b864a950463edabfba6a6b227466361
Eldar <eldarba@gmail.com>
2022-05-24 21:28:31 +0300

    jwt + filters

--- 12-04-22-spring-JWT-eldar/src/main/java/app/core/controllers/LoginController.java
+++ 12-04-22-spring-JWT-eldar/src/main/java/app/core/controllers/LoginController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
