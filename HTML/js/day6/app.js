adfd0557589a61424313ff6a7c1eae2d64822050
gold <narnavg@gmail.com>
2022-06-17 13:19:20 +0300

    JS day 6

---
+++ HTML/js/day6/app.js
@@ -0,0 +1,6 @@
+import x from './moduleA.js';
+import {y as a, z as b, sum} from './moduleA.js';
+console.log(x);
+console.log(a);
+console.log(b);
+console.log(sum(1500, 300));
---
+++ HTML/js/day6/moduleA.js
@@ -0,0 +1,14 @@
+let x = 100;
+let y = 200;
+let z = 300;
+
+export function sum(a, b) {
+    return a + b;
+}
+
+// default export
+export default x;
+
+// named imports
+export {y, z};
+
