6542759ec533a74c7723d1096efede30a7e76028
gold <narnavg@gmail.com>
2022-08-23 20:47:51 +0300

    AAA

---
+++ service-b/src/main/java/app/core/web/ControllerB.java
@@ -0,0 +1,17 @@
+package app.core.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerB {
+	@Value("${spring.cloud.consul.discovery.instanceId}")
+	private String instanceId;
+	
+	@GetMapping("/service/b")
+	public String handleB() {
+		return "Service B - " + instanceId;
+	}
+
+}
