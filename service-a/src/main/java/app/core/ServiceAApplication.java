90a24a3213f64e7be70f7082466d853ff90058db
gold <narnavg@gmail.com>
2022-08-23 21:10:41 +0300

    AAA

--- service-a/src/main/java/app/core/ServiceAApplication.java
+++ service-a/src/main/java/app/core/ServiceAApplication.java
@@ -3,6 +3,8 @@ package app.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,11 @@ public class ServiceAApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- service-a/src/main/java/app/core/web/ControllerA.java
+++ service-a/src/main/java/app/core/web/ControllerA.java
@@ -1,14 +1,49 @@
 package app.core.web;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ControllerA {
 	
+	@Autowired
+	private RestTemplate template;
+	
+	// @Autowired
+	// private DiscoveryClient discoveryClient;
+	
+	@Autowired
+	private LoadBalancerClient lbClient;
+	
 	@GetMapping("/service/a")
 	public String handleA() {
-		return "Service A";
+		String serviceId = "service-b";
+		URI baseUri = getInstaceBaseUriLB(serviceId);
+		String url = baseUri + "/service/b";
+		String res = template.getForObject(url, String.class);
+		return "Service A calling B - " + res;
+	} 
+	
+	private URI getInstaceBaseUriLB(String serviceId) {
+		ServiceInstance serviceInstance = lbClient.choose(serviceId);
+		return serviceInstance.getUri();
 	}
+	
+	// NO LB
+//	private URI getInstaceBaseUri(String serviceId) {
+//		List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+//		for (ServiceInstance serviceInstance : instances) {
+//			System.out.println(serviceInstance);
+//		}
+//		return instances.get(0).getUri();
+//	}
 
 }
