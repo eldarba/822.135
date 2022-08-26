4e247369123bd4c78e265d8327c89d08ee0b05a9
gold <narnavg@gmail.com>
2022-08-26 13:02:05 +0300

    micro services

--- dashboard/src/main/java/app/core/DashboardApplication.java
+++ dashboard/src/main/java/app/core/DashboardApplication.java
@@ -2,8 +2,12 @@ package app.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableDiscoveryClient
 public class DashboardApplication {
 
 	public static void main(String[] args) {
